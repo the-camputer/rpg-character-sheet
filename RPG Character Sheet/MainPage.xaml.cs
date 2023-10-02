e5a31bd950df9e535c0d40cb79831625b54883dd
the-camputer <mynichor@gmail.com>
2023-10-01 20:42:34 -0400

    gh-1 setup basic Main page with logos and Create button

--- RPG Character Sheet/MainPage.xaml.cs
+++ RPG Character Sheet/MainPage.xaml.cs
@@ -9,16 +9,9 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnCreateClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            Console.WriteLine("Creating new character sheet");
         }
     }
 }
