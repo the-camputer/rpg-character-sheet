538dbee5ceae7b3a858594ec4001be3d5eb8daa0
the-camputer <mynichor@gmail.com>
2023-11-03 20:59:49 -0400

    gh-9 started working on unit tests

---
+++ RPGCharacterSheet.Tests/CharacterSheet/BlankAs0ConverterTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using RPGCharacterSheet.Services;
+
+namespace RPGCharacterSheet.Tests.CharacterSheet
+{
+    public class BlankAs0ConverterTest
+    {
+        [Fact]
+        public void Converter_Converts_Int_To_String()
+        {
+            BlankAs0Converter converter = new BlankAs0Converter();
+            object result = converter.Convert(0, null, null, CultureInfo.InvariantCulture);
+            Assert.Equal(typeof(string), result.GetType());
+            Assert.Equal("0", result);
+
+        }
+    }
+}
